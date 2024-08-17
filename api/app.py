75b907aeab2676b1ac0adaf1406110e7c09984c7
Joseph Kim <josek16@uci.edu>
2024-08-17 01:53:46 -0700

    :hammer: Fix CORS issue.

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 # Load map outcome model
 with open('./models/map_outcome_model.pkl', 'rb') as f:
